dae3d710cdb5aea0be2596063654aa257a8d66a2
marcelooliveira <mclricardo@gmail.com>
2017-12-26 17:24:05 -0200

    .

---
+++ Refactoring3/Refactoring3/Aula3/R51.ReplaceConstructorWithFactoryMethod/antes/Funcionario.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace refatoracao.R51.ReplaceConstructorWithFactoryMethod.antes
+{
+    class Programa
+    {
+        void Main()
+        {
+            Funcionario funcionario = new Funcionario("José da Silva", 1000);
+        }
+    }
+
+    class Funcionario
+    {
+        readonly string nome;
+        public string Nome => nome;
+
+        readonly decimal salario;
+        public decimal Salario => salario;
+
+        public Funcionario(string nome, decimal salario)
+        {
+            this.nome = nome;
+            this.salario = salario;
+
+            LancarRegistrosNoBancoDeDados(this);
+            GerarDocumentosFiscais(this);
+            EnviarEmailDeBoasVindas(this);
+        }
+
+        private void EnviarEmailDeBoasVindas(Funcionario funcionario)
+        {
+            //método criado apenas para ilustração
+        }
+
+        private void GerarDocumentosFiscais(Funcionario funcionario)
+        {
+            //método criado apenas para ilustração
+        }
+
+        private void LancarRegistrosNoBancoDeDados(Funcionario funcionario)
+        {
+            //método criado apenas para ilustração
+        }
+    }
+}
---
+++ Refactoring3/Refactoring3/Aula3/R51.ReplaceConstructorWithFactoryMethod/depois/Funcionario.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace refatoracao.R51.ReplaceConstructorWithFactoryMethod.depois
+{
+    class Programa
+    {
+        void Main()
+        {
+            Funcionario funcionario = Funcionario.CriarFuncionario("José da Silva", 1000);
+        }
+    }
+
+    class Funcionario
+    {
+        readonly string nome;
+        public string Nome => nome;
+
+        readonly decimal salario;
+        public decimal Salario => salario;
+
+        private Funcionario(string nome, decimal salario)
+        {
+            this.nome = nome;
+            this.salario = salario;
+        }
+
+        public static Funcionario CriarFuncionario(string nome, decimal salario)
+        {
+            Funcionario funcionario = new Funcionario(nome, salario);
+            LancarRegistrosNoBancoDeDados(funcionario);
+            GerarDocumentosFiscais(funcionario);
+            EnviarEmailDeBoasVindas(funcionario);
+            return funcionario;
+        }
+
+        private static void EnviarEmailDeBoasVindas(Funcionario funcionario)
+        {
+            //método criado apenas para ilustração
+        }
+
+        private static void GerarDocumentosFiscais(Funcionario funcionario)
+        {
+            //método criado apenas para ilustração
+        }
+
+        private static void LancarRegistrosNoBancoDeDados(Funcionario funcionario)
+        {
+            //método criado apenas para ilustração
+        }
+    }
+}

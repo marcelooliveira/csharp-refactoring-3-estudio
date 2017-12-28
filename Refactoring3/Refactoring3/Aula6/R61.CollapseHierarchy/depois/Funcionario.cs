7bd5c52b556dd83af02974193d16da816a154e2f
marcelooliveira <mclricardo@gmail.com>
2017-12-28 11:23:36 -0200

    .

---
+++ Refactoring3/Refactoring3/Aula6/R61.CollapseHierarchy/depois/Funcionario.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace refatoracao.R61.CollapseHierarchy.depois
+{
+    //Como não aprender orientação a objetos: Herança
+    //http://blog.caelum.com.br/como-nao-aprender-orientacao-a-objetos-heranca/
+    class Programa
+    {
+        void Main()
+        {
+            var vendedor = new Funcionario("Walter White", "555-12345", "666-65432");
+        }
+    }
+
+    class Funcionario
+    {
+        public string Nome { get; set; }
+        public string TelefoneFixo { get; set; }
+        public string Celular { get; set; }
+
+        public Funcionario(string nome, string telefoneFixo, string celular)
+        {
+            Nome = nome;
+            TelefoneFixo = telefoneFixo;
+            Celular = celular;
+        }
+    }
+}
---
+++ Refactoring3/Refactoring3/Aula6/R62.FormTemplateMethod/depois/Customer.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace refatoracao.R62.FormTemplateMethod.depois
+{
+    class Cliente
+    {
+        public Cliente()
+        {
+            var resumo = new Resumo(this).GetResumo();
+            var resumoHTML = new ResumoHTML(this).GetResumo();
+        }
+
+        private IList<Locacao> locacoes;
+        public IList<Locacao> Locacoes
+        {
+            get { return locacoes; }
+            set { locacoes = value; }
+        }
+
+        private double valorTotal;
+        public double ValorTotal
+        {
+            get { return valorTotal; }
+            set { valorTotal = value; }
+        }
+
+        private double pontosDeFidelidade;
+        public double PontosDeFidelidade
+        {
+            get { return pontosDeFidelidade; }
+            set { pontosDeFidelidade = value; }
+        }
+
+        private string nome;
+        public string Nome
+        {
+            get { return nome; }
+            set { nome = value; }
+        }
+    }
+
+    class Locacao
+    {
+        private Filme filme;
+
+        public Filme Filme
+        {
+            get { return filme; }
+            set { filme = value; }
+        }
+    }
+
+    class Filme
+    {
+        private string titulo;
+
+        public string Titulo
+        {
+            get { return titulo; }
+            set { titulo = value; }
+        }
+    }
+
+    abstract class BaseResumo
+    {
+        protected readonly Cliente cliente;
+
+        public BaseResumo(Cliente cliente)
+        {
+            this.cliente = cliente;
+        }
+
+        public string GetResumo()
+        {
+            var result = new StringBuilder();
+            result.AppendLine(GetTitulo());
+            foreach (var locacao in cliente.Locacoes)
+            {
+                result.AppendLine(GetDetalheLocacao(locacao));
+            }
+            result.AppendLine(GetTextoValorDevido());
+            result.AppendLine(GetTextoPontosDeFidelidade());
+            return result.ToString();
+        }
+
+        protected abstract string GetTextoPontosDeFidelidade();
+
+        protected abstract string GetTextoValorDevido();
+
+        protected abstract string GetDetalheLocacao(Locacao locacao);
+
+        protected abstract string GetTitulo();
+    }
+
+    class Resumo : BaseResumo
+    {
+        public Resumo(Cliente cliente) : base(cliente)
+        {
+        }
+
+        protected override string GetTextoPontosDeFidelidade()
+        {
+            return $"Você ganhou: { cliente.PontosDeFidelidade.ToString() } pontos";
+        }
+
+        protected override string GetTextoValorDevido()
+        {
+            return "Total devido: " + cliente.ValorTotal.ToString();
+        }
+
+        protected override string GetDetalheLocacao(Locacao locacao)
+        {
+            return "\t" + locacao.Filme.Titulo;
+        }
+
+        protected override string GetTitulo()
+        {
+            return "Resumo de locações de " + cliente.Nome;
+        }
+    }
+
+    class ResumoHTML : BaseResumo
+    {
+        public ResumoHTML(Cliente cliente) : base(cliente)
+        {
+        }
+
+        protected override string GetTextoPontosDeFidelidade()
+        {
+            return $"Você ganhou <em>{cliente.PontosDeFidelidade.ToString()}</em> pontos.";
+        }
+
+        protected override string GetTextoValorDevido()
+        {
+            return "<p>Total devido: <em>" + cliente.ValorTotal.ToString() + "</em></p>";
+        }
+
+        protected override string GetDetalheLocacao(Locacao locacao)
+        {
+            return locacao.Filme.Titulo + "<br/>";
+        }
+
+        protected override string GetTitulo()
+        {
+            return "<h1>Locações de <em>" + cliente.Nome + "</em></h1>";
+        }
+    }
+}

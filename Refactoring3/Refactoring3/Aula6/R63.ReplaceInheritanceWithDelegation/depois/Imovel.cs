2a71f746ddb32e4f45b10785c8bbdff138465db0
Alex Felipe <mclricardo@gmail.com>
2017-12-29 18:06:21 -0200

    Substituir Herança por Delegação

--- Refactoring3/Refactoring3/Aula6/R63.ReplaceInheritanceWithDelegation/depois/Imovel.cs
+++ Refactoring3/Refactoring3/Aula6/R63.ReplaceInheritanceWithDelegation/depois/Imovel.cs
@@ -10,7 +10,7 @@ namespace refatoracao.Parte3.Aula6.R63.ReplaceInheritanceWithDelegation.depois
         {
             var imovel =
                 new Imovel("Rua dos Bobos, No. 0", 100000,
-                            "Vinicius de Moraes", "123456789-00");
+                new Proprietario("Vinicius de Moraes", "123456789-00"));
         }
     }
 
@@ -35,16 +35,20 @@ namespace refatoracao.Parte3.Aula6.R63.ReplaceInheritanceWithDelegation.depois
         }
     }
 
-    class Imovel : Proprietario
+    class Imovel
     {
+        private Proprietario proprietario;
+        internal Proprietario Proprietario { get => proprietario; set => proprietario = value; }
+
         private readonly String endereco;
         private decimal valor;
 
-        public Imovel(string endereco, decimal valor, string nomeProprietario, string cpfProprietario)
-            : base(nomeProprietario, cpfProprietario)
+        public Imovel(string endereco, decimal valor, Proprietario proprietario)
         {
+            this.proprietario = proprietario;
             this.endereco = endereco;
             this.valor = valor;
         }
+
     }
 }
